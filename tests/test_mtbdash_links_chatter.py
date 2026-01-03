657551fd17920edb87ed70097c238542928a7d54
Kevin Wilson <kev1583@gmail.com>
2026-01-02 21:49:26 -0700

    adding more tests

--- tests/test_mtbdash_links_chatter.py
+++ tests/test_mtbdash_links_chatter.py
@@ -20,7 +20,36 @@ def test_get_ebike_chatter_link(page: Page):
 def test_get_repair_chatter_link(page: Page):
     page.goto("https://www.mtbdash.com/bikerepair")
 
-    expect(page.get_by_role("link", name="Link").first).to_be_visible()          
+    expect(page.get_by_role("link", name="Link").first).to_be_visible() 
+
+def test_get_general_chatter_link(page: Page):
+    page.goto("https://www.mtbdash.com/general_chatter")
+
+    expect(page.get_by_role("link", name="Link").first).to_be_visible()    
+
+def test_get_gear_chatter_link(page: Page):
+    page.goto("https://www.mtbdash.com/chatter?state=gear")
+
+    expect(page.get_by_role("link", name="Link").first).to_be_visible()           
+
+# def test_get_europe_conditions_link(page: Page):
+#     # todo
+#     page.goto("https://www.mtbdash.com")    
+#     page.get_by_role("link", name="Europe").nth(0).click()
+#     page.get_by_role("link", name="Trail Chatter").click()    
+#     page.screenshot(path="full_page_screenshot.png", full_page=True) 
+#     expect(page.locator('.more_chatter_div').first).to_have_text('condition updates')             
+
+# def test_get_canada_chatter_link(page: Page):
+#     # todo
+#     page.goto("https://www.mtbdash.com/chatter?state=canada") 
+#     page.get_by_role("link", name="Canada").nth(1).click()
+#     page.goto("https://www.mtbdash.com/canada")
+#     page.get_by_role("link", name="Canada").nth(0).click()     
+
+#     page.screenshot(path="full_page_screenshot.png", full_page=True) 
+
+#     expect(page.get_by_role("link", name="Link").first).to_be_visible()        
 
 def test_get_colorado_trail_chatter_link(page: Page):
     page.goto("https://www.mtbdash.com/colorado")
