6cbc028ca8dd412612f2bfc3db736515fdd7a598
Kevin Wilson <kev1583@gmail.com>
2026-01-03 20:30:33 -0700

    adding tests

---
+++ tests/test_mtbdash_links_nearby.py
@@ -0,0 +1,21 @@
+import re
+from playwright.sync_api import Page, expect
+
+# Check Some Links
+def test_get_things_to_do_link(page: Page):
+    page.goto("https://www.mtbdash.com/florida")
+
+    page.locator('.city_select').select_option('Miami')
+
+    # Click things to do
+    expect(page.get_by_role("link", name="Things to do")).to_be_visible()  
+    page.get_by_role("link", name="Things to do").click()
+
+    # Validate Bike Shop and Hotel
+    expect(page.get_by_role("link", name="Top 10 Hotels in Miami, FL | Hotels.com")).to_be_visible()    
+    expect(page.get_by_role("link", name="Elite Cycling & Fitness | Miami, FL Bike Shop")).to_be_visible()         
+
+def test_get_things_to_do_hiking_link(page: Page):
+    page.goto("https://www.mtbdash.com/things_to_do?city=Toronto&state=canada")
+
+    expect(page.get_by_role("link", name="Hiking")).to_be_visible()                   
