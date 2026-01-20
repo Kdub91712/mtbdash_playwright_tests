8bd464ba81aac77f602c0da57e9fbd4a96762583
Kevin Wilson <kev1583@gmail.com>
2026-01-20 00:13:24 -0700

    test fix

--- tests/test_mtbdash_titles.py
+++ tests/test_mtbdash_titles.py
@@ -24,5 +24,5 @@ def test_city_page_has_title(page: Page):
     page.goto("https://www.mtbdash.com/things_to_do?city=Boulder&state=colorado")
 
     # Expect a title "to contain" a substring.
-    expect(page).to_have_title(re.compile("MTB DASH - Things to do in Boulder Colorado"))    
+    expect(page).to_have_title(re.compile("MTB DASH - Boulder Colorado Things to do"))    
         
